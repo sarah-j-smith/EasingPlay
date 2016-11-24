e342c17bdbe0ba6d503cd9dd1c603102de1fbc97
Sarah Smith <sarah@smithsoft.com.au>
2016-11-24 11:48:11 +1000

    Import play project

--- EasingPlay/GameScene.swift
+++ EasingPlay/GameScene.swift
@@ -8,37 +8,48 @@
 
 import SpriteKit
 import GameplayKit
+import SpriteKitEasingSwift
 
 class GameScene: SKScene {
     
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    fileprivate var lastUpdateTime : TimeInterval = 0
+    fileprivate var label : SKLabelNode?
+    fileprivate var spinnyNode : SKShapeNode?
+    
     
     override func sceneDidLoad() {
 
         self.lastUpdateTime = 0
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
         // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
+        let w = (self.size.width + self.size.height) * 0.2
         self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
+        let easeTime = 4.0
+
+        let fn = SKEase.getEaseFunction(.curveTypeQuadratic, easeType: .easeTypeInOut)
+        let easeFat = SKEase.createFloatTween(2.5, end: 30.0, time: easeTime/2, easingFunction: fn) { (node: SKNode, param: CGFloat) in
+            let spinny = node as! SKShapeNode
+            spinny.lineWidth = param
+        }
+        let easeThin = SKEase.createFloatTween(30.0, end: 2.5, time: easeTime/2, easingFunction: fn) { (node: SKNode, param: CGFloat) in
+            let spinny = node as! SKShapeNode
+            spinny.lineWidth = param
+        }
+
         if let spinnyNode = self.spinnyNode {
             spinnyNode.lineWidth = 2.5
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
+            let rotate = SKAction.rotate(byAngle: CGFloat(M_PI) * CGFloat(2.0), duration: easeTime)
+            let easeFatThin = SKAction.sequence([easeFat, easeThin])
+            let rotateAndEase = SKAction.group([rotate, easeFatThin])
+            
+            spinnyNode.run(SKAction.repeatForever(rotateAndEase))
+            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: easeTime),
+                                              SKAction.fadeOut(withDuration: easeTime),
                                               SKAction.removeFromParent()]))
         }
     }
@@ -47,49 +58,19 @@ class GameScene: SKScene {
     func touchDown(atPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
+            n.strokeColor = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: [SKColor.green, SKColor.red, SKColor.orange, SKColor.blue, SKColor.cyan]).first as! SKColor
             self.addChild(n)
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
     
     
     override func update(_ currentTime: TimeInterval) {
+        
         // Called before each frame is rendered
         
         // Initialize _lastUpdateTime if it has not already been
@@ -99,7 +80,7 @@ class GameScene: SKScene {
         
         // Calculate time since last update
         let dt = currentTime - self.lastUpdateTime
-        
+
         // Update entities
         for entity in self.entities {
             entity.update(deltaTime: dt)
---
+++ Podfile
@@ -0,0 +1,7 @@
+target 'EasingPlay' do
+
+    platform :ios, '10.0'
+    use_frameworks!
+
+    pod 'SpriteKitEasingSwift'
+end
